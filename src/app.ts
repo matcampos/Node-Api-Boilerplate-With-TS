e50270f1ed7a0d7ac8b05c1e1a0e259a1df04fa7
matcampos <matheus.alves1998@hotmail.com>
2021-03-03 16:59:01 -0300

    Removing dotenv import from app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
 import config from './config/config';
 import app from './config/express';
-import dotenv from 'dotenv';
-dotenv.config();
 
 app.listen(config.port, () => {
     console.info(`server started on port ${config.port} (${config.env})`); // eslint-disable-line no-console
